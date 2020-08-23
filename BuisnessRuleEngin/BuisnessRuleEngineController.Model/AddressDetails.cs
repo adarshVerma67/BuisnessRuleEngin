c9eed2af020b7930ff604cf049b6cbf9bf3332fb
adarsh <adarsh.tashu@gmail.com>
2020-08-24 01:02:52 +0530

    updated data annotation

--- BuisnessRuleEngin/BuisnessRuleEngineController.Model/AddressDetails.cs
+++ BuisnessRuleEngin/BuisnessRuleEngineController.Model/AddressDetails.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BuisnessRuleEngineControllers.Model
 {
     public class AddressDetails
     {
+        [Required]
         public int DoorNumber { get; set; }
 
+        [Required]
         public String Address { get; set; }
 
+        [Required]
         public String PinCode { get; set; }
 
     }
